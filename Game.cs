51987820a677e45ea542bd09b0a54513c58fe910
Elias Holter <eliasholter1@gmail.com>
2019-11-21 11:51:18 -0600

    Pre-Lunch Commit, working on allowing player to select number of rounds

--- Game.cs
+++ Game.cs
@@ -10,6 +10,7 @@ namespace RPSLSGame
     {
         int scoreOne;
         int scoreTwo;
+        int roundsToPlay;
         string choiceOne;
         string choiceTwo;
         string whoToPlay;
@@ -26,72 +27,21 @@ namespace RPSLSGame
             validInput = false;
             playAgain = true;
             player1 = new Human();
-
-            //Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
-            //whoToPlay = Console.ReadLine().ToLower();
-
-            //while (validGameStart == false)
-            //{
-            //    switch (whoToPlay)
-            //    {
-            //        case "friend":
-            //            Console.Clear();
-            //            validGameStart = true;
-            //            player2 = new Human();
-            //            break;
-            //        case "computer":
-            //            Console.Clear();
-            //            validGameStart = true;
-            //            player2 = new AI();
-            //            break;
-            //        default:
-            //            Console.Clear();
-            //            Console.WriteLine("That is not a valid selection.");
-            //            Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
-            //            whoToPlay = Console.ReadLine().ToLower();
-            //            break;
-            //    }
-            //}
         }
 
         public void RunGame()
         {
             while (playAgain == true)
             {
-                Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
-                whoToPlay = Console.ReadLine().ToLower();
-
-                while (validInput == false)
-                {
-                    switch (whoToPlay)
-                    {
-                        case "friend":
-                            Console.Clear();
-                            validInput = true;
-                            player2 = new Human();
-                            break;
-                        case "computer":
-                            Console.Clear();
-                            validInput = true;
-                            player2 = new AI();
-                            break;
-                        default:
-                            Console.Clear();
-                            Console.WriteLine("That is not a valid selection.");
-                            Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
-                            whoToPlay = Console.ReadLine().ToLower();
-                            break;
-                    }
-                }
-
-                validInput = false;
+                SelectGameMode();
+                SelectGameRounds();
 
                 player1.ChooseName();
                 Console.Clear();
                 player2.ChooseName();
                 Console.Clear();
 
-                while (scoreOne < 3 && scoreTwo < 3)
+                while (scoreOne < roundsToPlay && scoreTwo < roundsToPlay)
                 {
                     Console.WriteLine(player1.name + " Score: " + scoreOne + "   " + player2.name + " Score: " + scoreTwo + "\n\n");
                     Console.WriteLine(player1.name + "'s turn\n\n");
@@ -107,32 +57,13 @@ namespace RPSLSGame
 
                     Console.WriteLine(choiceOne + " VS. " + choiceTwo + "\n\n");
 
-                    if ((choiceOne == "rock" && choiceTwo == "scissors") || (choiceOne == "scissors" && choiceTwo == "paper") || (choiceOne == "paper" && choiceTwo == "rock") || (choiceOne == "rock" && choiceTwo == "lizard") || (choiceOne == "lizard" && choiceTwo == "spock") || (choiceOne == "spock" && choiceTwo == "scissors") || (choiceOne == "scissors" && choiceTwo == "lizard") || (choiceOne == "lizard" && choiceTwo == "paper") || (choiceOne == "paper" && choiceTwo == "spock") || (choiceOne == "spock" && choiceTwo == "rock"))
-                    {
-                        scoreOne += 1;
-                        Console.WriteLine(player1.name + " Wins The Round!");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    else if (choiceOne == choiceTwo)
-                    {
-                        Console.WriteLine("The Round Ended In A Tie!");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        scoreTwo += 1;
-                        Console.WriteLine(player2.name " Wins The Round!");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
+                    DetermineRoundWinner();
                 }
 
                 if (scoreOne > scoreTwo)
                 {
                     Console.Clear();
-                    Console.WriteLine(player1.name " Wins The Game!");
+                    Console.WriteLine(player1.name + " Wins The Game!");
                     Console.ReadLine();
                 }
                 else
@@ -142,34 +73,116 @@ namespace RPSLSGame
                     Console.ReadLine();
                 }
 
-                while (validInput == false)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Would you like to play again? Please enter yes or no:");
-                    string replayDescision = Console.ReadLine();
-
-                    switch (replayDescision)
-                    {
-                        case "yes":
-                            scoreOne = 0;
-                            scoreTwo = 0;
-                            validInput = true;
-                            player1 = new Human();
-                            break;
-                        case "no":
-                            validInput = true;
-                            playAgain = false;
-                            break;
-                        default:
-                            Console.WriteLine("Invalid input, please try again.");
-                            Console.ReadLine();
-                            Console.Clear();
-                            break;
-                    }
-                }
-
-                validInput = false;
+                OfferToPlayAgain();
             }
+        }
+
+        public void SelectGameMode()
+        {
+            Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
+            whoToPlay = Console.ReadLine().ToLower();
+
+            while (validInput == false)
+            {
+                switch (whoToPlay)
+                {
+                    case "friend":
+                        Console.Clear();
+                        validInput = true;
+                        player2 = new Human();
+                        break;
+                    case "computer":
+                        Console.Clear();
+                        validInput = true;
+                        player2 = new AI();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("That is not a valid selection.");
+                        Console.WriteLine("Who would you like to play? Please enter 'friend' or 'computer':");
+                        whoToPlay = Console.ReadLine().ToLower();
+                        break;
+                }
+            }
+
+            validInput = false;
+        }
+
+        public void SelectGameRounds()
+        {
+            while (validInput == false)
+            {
+                Console.WriteLine("Please select the amount of rounds you would like to play (Must be greater than 3):");
+                roundsToPlay = Int32.Parse(Console.ReadLine());
+
+                if (roundsToPlay.GetType() == typeof(int))
+                {
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please try again.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+            }
+        }
+
+        public void DetermineRoundWinner()
+        {
+            if ((choiceOne == "rock" && choiceTwo == "scissors") || (choiceOne == "scissors" && choiceTwo == "paper") || (choiceOne == "paper" && choiceTwo == "rock") || (choiceOne == "rock" && choiceTwo == "lizard") || (choiceOne == "lizard" && choiceTwo == "spock") || (choiceOne == "spock" && choiceTwo == "scissors") || (choiceOne == "scissors" && choiceTwo == "lizard") || (choiceOne == "lizard" && choiceTwo == "paper") || (choiceOne == "paper" && choiceTwo == "spock") || (choiceOne == "spock" && choiceTwo == "rock"))
+            {
+                scoreOne += 1;
+                Console.WriteLine(player1.name + " Wins The Round!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else if (choiceOne == choiceTwo)
+            {
+                Console.WriteLine("The Round Ended In A Tie!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+            else
+            {
+                scoreTwo += 1;
+                Console.WriteLine(player2.name + " Wins The Round!");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+
+        public void OfferToPlayAgain()
+        {
+            while (validInput == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Would you like to play again? Please enter yes or no:");
+                string replayDescision = Console.ReadLine();
+
+                switch (replayDescision)
+                {
+                    case "yes":
+                        scoreOne = 0;
+                        scoreTwo = 0;
+                        validInput = true;
+                        player1 = new Human();
+                        Console.Clear();
+                        break;
+                    case "no":
+                        validInput = true;
+                        playAgain = false;
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid input, please try again.");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
+
+            validInput = false;
         }
     }
 }
